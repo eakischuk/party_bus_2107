7b92b16b680fd983d75c809d8af03f05f06e531d
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-07 14:54:49 -0600

    Add passengers array and method

--- lib/bus.rb
+++ lib/bus.rb
@@ -1,9 +1,10 @@
 class Bus
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :passengers
 
   def initialize (name, capacity)
     @name = name
     @capacity = capacity
+    @passengers = []
   end
 
 end
